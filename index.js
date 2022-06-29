da0ee701e759a4c33ae336237527790b45bf63e5
Nathan Westergard <m800mazdarx8@gmail.com>
2022-06-29 15:06:15 -0600

    adding more functions

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const schedule = document.querySelector('#schedule')
 const pay = document.querySelector('#pay')
+const about = document.querySelector('#about')
+const services = document.querySelector('#services')
+const clients = document.querySelector('#clients')
+const contact = document.querySelector('#contact')
+
 
 const error = () => {
     alert('Sorry that has not been made yet')
@@ -11,3 +16,7 @@ const calender = () => {
 
 pay.addEventListener('click', error)
 schedule.addEventListener('click', calender)
+about.addEventListener('click', error)
+services.addEventListener('click', error)
+clients.addEventListener('click', error)
+contact.addEventListener('click', error)
