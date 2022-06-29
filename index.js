2134b25cf46157c2418c911ae624f8930b9513e1
Nathan Westergard <m800mazdarx8@gmail.com>
2022-06-29 14:26:51 -0600

    Adding stuff

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const schedule = document.querySelector('#schedule')
+const pay = document.querySelector('#pay')
 
 const error = () => {
     alert('Sorry that has not been made yet')
 }
 
 schedule.addEventListener('click', error)
+pay.addEventListener('click', error)
