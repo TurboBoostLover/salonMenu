84e0e17e41ef04359af0ba738843e33742e09c23
Nathan Westergard <m800mazdarx8@gmail.com>
2022-06-30 14:01:47 -0600

    Added rollbar

--- Server/index.js
+++ Server/index.js
@@ -3,6 +3,17 @@ const path = require('path')
 
 const app = express()
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'c0e7a2d7410049cea6702e3cc737f36f',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const schedule = document.querySelector('#schedule')
 const pay = document.querySelector('#pay')
-//const about = document.querySelector('#about')
-//const services = document.querySelector('#serve')
-//const clients = document.querySelector('#clients')
-//const contact = document.querySelector('#contact')
+
 
 
 const error = () => {
@@ -20,7 +17,3 @@ const payMe = () => {
 
 pay.addEventListener('click', payMe)
 schedule.addEventListener('click', calender)
-//about.addEventListener('click', error)
-//services.addEventListener('click', error)
-//clients.addEventListener('click', error)
-//contact.addEventListener('click', error)
