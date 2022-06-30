fb25dd0b31c4abb083c0bc32554eb92024e6d71f
Nathan Westergard <m800mazdarx8@gmail.com>
2022-06-30 14:22:46 -0600

    bugs

--- index.js
+++ index.js
@@ -2,26 +2,16 @@ const schedule = document.querySelector('#schedule')
 const pay = document.querySelector('#pay')
 const test = document.querySelector('#test')
 
-// include and initialize the rollbar library with your access token
-var Rollbar = require('rollbar')
-var rollbar = new Rollbar({
-  accessToken: 'c0e7a2d7410049cea6702e3cc737f36f',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-})
 
 const error = () => {
-    rollbar.error('Big error they got into')
     alert('Sorry that has not been made yet')
 }
 
 const calender = () => {
-    rollbar.info('Some one trying to schedule')
     window.location.href=("https://calendar.google.com/calendar")
 }
 
 const payMe = () => {
-    rollbar.info('Some one is paying!')
     window.location.href=("https://venmo.com/Nathan-Westergard")
 }
 
