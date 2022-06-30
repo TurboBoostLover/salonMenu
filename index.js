76bb0f7bae930a8f18c744093ea65d7f90234265
Nathan Westergard <m800mazdarx8@gmail.com>
2022-06-30 14:31:55 -0600

    bugs

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const schedule = document.querySelector('#schedule')
 const pay = document.querySelector('#pay')
 const test = document.querySelector('#test')
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'fefb3ab4b78342ab98bc8a1af3e4f3cc',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 const error = () => {
+    rollbar.error('Someone sent an error.')
     alert('Sorry that has not been made yet')
 }
 
