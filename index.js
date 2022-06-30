d694ee8de5540ce7ccbb146ac3d11402a123e999
Nathan Westergard <m800mazdarx8@gmail.com>
2022-06-30 14:11:23 -0600

    Adding rollbar

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const schedule = document.querySelector('#schedule')
 const pay = document.querySelector('#pay')
+const test = document.querySelector('#test')
 
 
 
 const error = () => {
+    rollbar.error('Big error they got into')
     alert('Sorry that has not been made yet')
 }
 
 const calender = () => {
+    rollbar.info('Some one trying to schedule')
     window.location.href=("https://calendar.google.com/calendar")
 }
 
 const payMe = () => {
+    rollbar.info('Some one is paying!')
     window.location.href=("https://venmo.com/Nathan-Westergard")
 }
 
 pay.addEventListener('click', payMe)
 schedule.addEventListener('click', calender)
+test.addEventListener('click'. error)
